4b02e4e3c2c2bf7091e82ee3e11b1ed0b4dd3963
Aniket <aniketkumarpaul1@gmail.com>
2022-06-14 10:29:13 +0530

    stacks

---
+++ STACK_Balanced_Parenthesis.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isValid(string s) {
+    int n = s.size();
+
+    stack<char> st;
+    bool ans = true;
+
+    for(int i=0; i<n; i++) {
+        if(s[i]=='{' or s[i]=='(' or s[i]=='[') {
+            st.push(s[i]);
+        } else if (s[i]==')'){
+            if(!st.empty() && st.top()=='(')
+                st.pop();
+            else{
+                ans = false;
+                break;
+            }
+        } else if (s[i]=='}'){
+            if(!st.empty() && st.top()=='{')
+                st.pop();
+            else{
+                ans = false;
+                break;
+            }
+        } else if (s[i]==']'){
+            if(!st.empty() && st.top()=='[')
+                st.pop();
+            else{
+                ans = false;
+                break;
+            }
+        }
+    }
+
+    if(!st.empty())
+        return false;
+    return ans;
+}
+
+int main() {
+    string s = "{([])}";
+
+    if(isValid(s)) {
+        cout<<"Balanced Parenthesis"<<endl;
+    } else {
+        cout<<"Unbalanced Parenthesis"<<endl;
+    }
+    return 0;
+}
